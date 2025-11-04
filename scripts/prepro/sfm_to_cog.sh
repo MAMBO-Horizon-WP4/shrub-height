b30ba941440eb8bd476264eb4a31967fed2e038d
Jo Walsh <metazool@fastmail.net>
2025-11-04 12:16:46 +0000

    accept inputs in COG conversion wrapper script

--- scripts/prepro/sfm_to_cog.sh
+++ scripts/prepro/sfm_to_cog.sh
@@ -1,8 +1,12 @@
-cd data
+# Conversion into Cloud Optimised GeoTIFF
 
+input_file=${1:-raw/SfM/StrawDSM_SfM_L1-geoid_apr24.tif}
+output_file=${2:-interim/rgb_sfm.tif}
+
+echo $input_file
+
+echo $output_file
 # Define common creation options
 co_params="-of COG -co BLOCKSIZE=256 -co COMPRESS=DEFLATE -co PREDICTOR=2 -co BIGTIFF=YES"
 
-# gdalwarp -t_srs EPSG:27700 raw/SfM/StrawDSM_SfM_L1-geoid_apr24.tif $co_params interim/dsm_sfm.tif
-
-gdalwarp -t_srs EPSG:27700 raw/SfM/strawberry_ortho_terraSfM_L1_apr24.tif $co_params interim/rgb_sfm.tif
+gdalwarp -t_srs EPSG:27700 $input_file $co_params $output_file
